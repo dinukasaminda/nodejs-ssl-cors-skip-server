578e9d54b4a512ec3ea93cb9e3ad883345d7a79c
mdilshan <mdilshan21@gmail.com>
2020-09-13 20:22:53 +0530

    fix : cors fix

--- index.js
+++ index.js
@@ -1,59 +1,26 @@
-const baseUrl = "https://192.248.22.198";
+const baseUrl = 'https://192.248.22.198';
 //http://localhost:5000/api/accounts
 
-const express = require("express");
-const http = require("http");
+const express = require('express');
+const http = require('http');
 const app = express();
-const bodyParser = require("body-parser");
-const axios = require("axios").default;
-const fs = require("fs");
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// );
+const bodyParser = require('body-parser');
+const axios = require('axios').default;
+const fs = require('fs');
+const cors = require('cors');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-// app.use(bodyParser.json());
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-// //app.use(express.static(path.join(__dirname, "public")));
 const PORT = 5000;
 const server = http.Server(app);
 server.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
 
-// app.use(async (req, res, next) => {
-//   try {
-//     console.log(baseUrl + req.url);
-//     const response = await axios.post(baseUrl + req.url, {
-//       headers: {
-//         bearer: req.headers["bearer"],
-//       },
-//       rejectUnauthorized: false,
-//     });
-//     console.log(response);
-//     res.send(response.data).status(200);
-//     return;
-//   } catch (err) {
-//     console.error(err);
-//     res.send({ err: err }).status(400);
-//     return;
-//   }
-// });
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
+app.use(cors());
 app.use(
-  "/api",
+  '/api',
   createProxyMiddleware({
     target: baseUrl,
     changeOrigin: true,
